2434698127b2616475dad219d5a958fe51793e6f
DillionApple <DillionMango@gmail.com>
2018-08-20 22:38:05 +0800

    add canal images

--- main.sh
+++ main.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 K8S_VERSION=1.11.2
+DOCKER_REGISTRY_URL=dillionmango/k8s_images
 
 images=$(kubeadm config --kubernetes-version $K8S_VERSION images list)
 
@@ -18,12 +19,28 @@ function generate_docker_files() {
     done
 }
 
+function pull_images() {
+    for image in $images
+    do
+	full_name=$(echo $image | awk -F/ '{ print $2 }')
+	image_name=$(echo $full_name | awk -F: '{ print $1 }')
+	tag_name=$(echo $full_name | awk -F: '{ print $2 }')
+	folder_name=$image_name-$tag_name
+	docker pull $DOCKER_REGISTRY_URL:$folder_name
+	docker tag $DOCKER_REGISTRY_URL:$folder_name $image
+	docker rmi $DOCKER_REGISTRY_URL:$folder_name
+    done
+}
+
 
 case $1 in
     generate_docker_files)
 	generate_docker_files
 	;;
+    pull_images)
+	pull_images
+	;;
     *)
-	echo usage: generate_docker_files/
+	echo usage: generate_docker_files/pull_images
 esac
 
